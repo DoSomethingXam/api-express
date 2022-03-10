a2e9c5adf91ab14d9555296c9582e171dcce5e15
TuanAnh <tuananhnguyen6798@gmail.com>
2022-03-11 01:32:59 +0700

    Change index

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(swagger);
 
-app.get('/', () => {
+app.get('/', (req, res) => {
   res.json({
     status: 'success',
     message: 'Hello World!',
